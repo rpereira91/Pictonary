4e87c7888fa4f55cf78a7c7e5915b8a5b05a5d42
Unknown <Rp09rl@brocku.ca>
2018-12-20 11:15:12 -0500

    added hue control

---
+++ hue_control.py
@@ -0,0 +1,16 @@
+from phue import Bridge
+
+class HueControl:
+    
+    def __init__(self,IP):
+        self.b = Bridge(IP)
+        # If the app is not registered and the button is not pressed, press the button and call connect() (this only needs to be run a single time)
+        self.b.connect()
+        # Get the bridge state (This returns the full dictionary that you can explore)
+        self.b.get_api()
+
+    def flash_blubs(self):
+        self.b.set_light(3,'on',False)
+        self.b.set_light(3,'on',True)
+        self.b.set_light(3,'on',False)
+        self.b.set_light(3,'on',True)
