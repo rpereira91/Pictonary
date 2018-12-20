e27442184d79a68dcd3e16529b810a040a1f6440
Unknown <Rp09rl@brocku.ca>
2018-12-20 10:53:07 -0500

    added a soft timer

--- generate_word.py
+++ generate_word.py
@@ -1,9 +1,7 @@
-from flask import Flask, render_template,request
-import random
-from flask_bootstrap import Bootstrap
+from flask import Flask, render_template,request,redirect,url_for
+import random,time
+from apscheduler.schedulers.background import BackgroundScheduler
 app = Flask(__name__)
-Bootstrap(app)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def method_name():
@@ -14,6 +12,11 @@ def method_name():
     'HOTS']
     if request.method=='POST':
         difficulty = request.form.get('difficulty')
+        scheduler.remove_all_jobs()
+        start = start_time()
+        scheduler.add_job(check_time, 'interval', seconds=1,args=(start,))
+        if not scheduler.running:
+            scheduler.start()
         return render_template('index.html',diff=diff,selected=difficulty, word=get_word(difficulty))
     return render_template('index.html',diff=diff)
     
@@ -21,8 +24,17 @@ def get_word(difficulty):
     file_path = "word-lists/" + difficulty.lower()+".txt"
     text_file =  open(file_path,'r')
     words = text_file.read().split('\n')
+    start_time()
     return words[random.randint(0,len(words)-1)]
 
+def check_time(s_time):
+    if (time.time()-s_time) > 10:
+        print("TEST")
+        scheduler.remove_all_jobs()
+def start_time():
+    return time.time()
+
 if __name__ == '__main__':
-  app.run(host= '127.0.0.1', port=8000, debug=True)
+    scheduler = BackgroundScheduler()
+    app.run(host= '127.0.0.1', port=8000, debug=True)
  
